b258344f0aabd026c539e96139c9fb33f7c9f7dd
404151146@qq.com <404151146@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-02-27 10:15:45 +0000

    commit
    git-svn-id: svn://svn3.apicloud.com/A6064800190587/bilika@17 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/common.js
+++ script/common.js
@@ -12,7 +12,7 @@ var footer, footerHeight = 0
 function fnReadyHeader() {
     header = $api.byId('header')
     if (header) {
-        $api.fixIos7Bar(header))
+        $api.fixIos7Bar(header)
         headerHeight = $api.offset(header).h
     }
 }
@@ -46,12 +46,14 @@ function fnReadyKeyback() {
 function fnReadyOpenWin() {
     var buttons = $api.domAll('.open-win')
     for (var i = 0; i < buttons.length; i++) {
-        $api.attr(buttons, 'tapmode', 'highlight')
+        $api.attr(buttons[i], 'tapmode', 'highlight')
         buttons[i].onclick = function() {
             var target = $api.closest(event.target, '.open-win')
             var winName = $api.attr(target, 'win')
-            var isNeedLogin = $api.attr(target, 'win')
+            var isNeedLogin = $api.attr(target, 'login')
+            var isCommon = $api.attr(target, 'common')
             var params = $api.attr(target, 'params')
+            var url = ''
 
             if (isNeedLogin && !$api.getStorage('token')) {
                 winName = 'login'
@@ -61,30 +63,42 @@ function fnReadyOpenWin() {
                 params = JSON.parse(params)
             }
 
+            // 判断是否是通用的子页面来确定 url
+            url = isCommon ? '../' + winName + '.html' : './' + winName + '.html'
+
             api.openWin({
                 name: winName,
-                url: './' + winNAme + '.html',
-                pageParam: params
-            })
+                url: url,
+                pageParam: {
+                    name: 'test'
+                },
+                slidBackEnabled: true, // 是否支持滑动返回
+            });
+
         }
     }
     api.parseTapmode()
 }
 
 // 打开 frame
-function fnReadyFrame() {
+function fnReadyFrame(params) {
     var frameName = api.winName + '_frame'
+    if (params) {
+        var bgColor = params.bgColor ? params.bgColor : '#fff';
+    }
     api.openFrame({
         name: frameName,
-        url: './' + frameName + './html',
+        url: './' + frameName + '.html',
         bounces: true,
-        bgColor: '#fff',
+        bgColor: bgColor,
         rect: {
             x: 0,
             y: headerHeight,
             w: 'auto',
             h: api.winHeight - headerHeight - footerHeight
-        }
-        pageParam: api.pageParam
+        },
+        pageParam: api.pageParam,
+        // vScrollBarEnabled: true,
+        // hScrollBarEnabled: true
     })
 }
